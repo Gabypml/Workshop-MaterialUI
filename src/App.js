339fc7273bb59620e5cebce8b96e2ecfd36b7936
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-03-06 12:24:25 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Link, useRouteMatch } from "react-router-dom";
+import "./css/App.min.css";
+import Metronome from "./metronome";
+
+import metronomeImg from "./assets/metronome.svg";
+import systemeImg from "./assets/systeme.svg";
+import loveImg from "./assets/love.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          React + Electron = <img src={loveImg} />
+        </h1>
+      </header>
+      <main>
+        <Router>
+          <nav className="navbar">
+            <div className="container-link">
+              <Link className="nav-link" to="/metronome">
+                Metronome
+                <img src={metronomeImg} />
+              </Link>
+            </div>
+            <div className="container-link">
+              <Link className="nav-link" to="/systemespecs">
+                Systemespecs
+                <img src={systemeImg} />
+              </Link>
+            </div>
+          </nav>
+          <Route path="/metronome">
+            <Metronome />
+          </Route>
+        </Router>
+      </main>
+    </div>
+  );
+}
+
+export default App;
