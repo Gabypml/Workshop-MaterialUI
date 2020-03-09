57447c07f67bf27b57643e1f8d0c813b634d67e0
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-03-09 10:59:34 +0100

    add MUI styles

--- src/App.js
+++ src/App.js
@@ -12,41 +12,47 @@ import metronomeImg from "./assets/metronome.svg";
 import systemeImg from "./assets/systeme.svg";
 import loveImg from "./assets/love.svg";
 import { Grid } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
 
 function App() {
   return (
-    <Grid container>
-      <Grid item xs={"12"}>
-        <header className="App-header">
-          <h1>
-            React + Electron = <img src={loveImg} />
-          </h1>
-        </header>
+    <Paper
+      elevation={"3"}
+      style={{ backgroundColor: "#ccc", marginTop: "10px" }}
+    >
+      <Grid container>
+        <Grid item xs={"12"}>
+          <header>
+            <h1>
+              React + Electron = <img src={loveImg} />
+            </h1>
+          </header>
+        </Grid>
+        <Grid item xs={"12"}>
+          <main>
+            <Router>
+              <nav className="navbar">
+                <Grid item xs={"3"} className="container-link">
+                  <Link className="nav-link" to="/metronome">
+                    Metronome
+                    <img src={metronomeImg} />
+                  </Link>
+                </Grid>
+                <Grid item xs={"3"} className="container-link">
+                  <Link className="nav-link" to="/systemespecs">
+                    Systemespecs
+                    <img src={systemeImg} />
+                  </Link>
+                </Grid>
+              </nav>
+              <Route path="/metronome">
+                <Metronome />
+              </Route>
+            </Router>
+          </main>
+        </Grid>
       </Grid>
-      <Grid item xs={"12"}>
-        <main>
-          <Router>
-            <nav className="navbar">
-              <Grid item className="container-link">
-                <Link className="nav-link" to="/metronome">
-                  Metronome
-                  <img src={metronomeImg} />
-                </Link>
-              </Grid>
-              <Grid item className="container-link">
-                <Link className="nav-link" to="/systemespecs">
-                  Systemespecs
-                  <img src={systemeImg} />
-                </Link>
-              </Grid>
-            </nav>
-            <Route path="/metronome">
-              <Metronome />
-            </Route>
-          </Router>
-        </main>
-      </Grid>
-    </Grid>
+    </Paper>
   );
 }
 
